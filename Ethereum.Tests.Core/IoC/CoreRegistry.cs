bf182b7b40471601c7fc8f5ede260defc6146bc3
Ciaran < ciaran.jones99@gmail.com>
2015-04-02 20:02:27 +0100

    StructureMap setup

---
+++ Ethereum.Tests.Core/IoC/CoreRegistry.cs
@@ -0,0 +1,16 @@
+﻿using Ethereum.Core;
+using Ethereum.Network;
+using StructureMap.Configuration.DSL;
+
+namespace Ethereum.Tests.IoC
+{
+    public class CoreRegistry : Registry
+    {
+        public CoreRegistry()
+        {
+            For<ISubscriptionService>().Use<SubscriptionService>();
+            For<IEventPublisher>().Use<EventPublisher>();
+            For<IPeerDataStore>().Singleton().Use<PeerDataStore>();
+        }
+    }
+}
